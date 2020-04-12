6bc1481a7bcdbe5c8bafd7aeeeeaff28e48d0cb8
maple3142 <kirby741852963@gmail.com>
2020-04-12 19:27:07 +0800

    fix headless

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import puppeteer from 'puppeteer-extra'
 import StealthPlugin = require('puppeteer-extra-plugin-stealth')
 import path = require('path')
-import { Browser } from 'puppeteer'
+import { Browser, Page } from 'puppeteer'
 import yargs = require('yargs')
 import perkUpgrade, { DefaultUpgradingStrategy } from './perk-upgrade'
+import { writeFile, readFile, pathExists } from 'fs-extra'
 
 const argv = yargs
 	.locale('en')
@@ -22,21 +23,38 @@ const argv = yargs
 	}).argv
 
 type LoginMethod = 'facebook' | 'google' | 'vk'
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 let browser: Browser
 ;(async () => {
-	const userDataDir = path.join(__dirname, '../user_data')
+	console.log('Bot: starting browser')
 	browser = await puppeteer.use(StealthPlugin()).launch({
-		headless: argv.headless,
-		args: [`--user-data-dir=${userDataDir}`]
+		headless: argv.headless
 	})
+	console.log('Bot: browser started')
 	browser.on('disconnected', () => {
 		console.log('Bot: browser closed')
 		process.exit()
 	})
 	process.on('SIGINT', () => browser.close())
 	const page = await browser.newPage()
-	async function login() {
+	async function persistCookies(page: Page, cookiePath: string) {
+		const cookies = await page.cookies(
+			'https://rivalregions.com/',
+			'https://www.facebook.com/',
+			'https://accounts.google.com/',
+			'https://oauth.vk.com/'
+		)
+		await writeFile(cookiePath, JSON.stringify(cookies))
+		console.log('Cookie: successfully wrote cookies to cookies.json')
+	}
+	async function loadCookies(page: Page, cookiePath: string) {
+		if (await pathExists(cookiePath)) {
+			const cookies = JSON.parse(await readFile(cookiePath, 'utf-8'))
+			await page.setCookie(...cookies)
+			console.log('Cookie: successfully loaded from cookies.json')
+		}
+	}
+	async function login(page: Page) {
 		const selectors = {
 			facebook: '.sa_link[href*="facebook.com"]',
 			google: '.sa_link[href*="google.com"]',
@@ -52,25 +70,34 @@ let browser: Browser
 			console.log('Login: not logged in, manual login required')
 			const href = (await page.$eval(
 				selectors[argv.login as LoginMethod],
-				el => el.getAttribute('href')
+				(el) => el.getAttribute('href')
 			)) as string
 			await page.goto(href, {
 				waitUntil: 'networkidle0'
 			})
-			await page.waitForSelector('#header_my_avatar', {
-				timeout: 120 * 1000
+			await page.waitForResponse('https://rivalregions.com/', {
+				timeout: 0
 			})
+			await page.waitFor('#chat input[name=name]')
 			console.log('Login: manual login success')
+			return true // true means manual login is used
 		}
+		return false
 	}
-	await login()
-	const userName = await page.$eval('#chat input[name=name]', el =>
+	const cookiePath = path.join(__dirname, '../cookies.json')
+	await loadCookies(page, cookiePath)
+	if (await login(page)) {
+		await persistCookies(page, cookiePath)
+	}
+	const userName = await page.$eval('#chat input[name=name]', (el) =>
 		el.getAttribute('value')
 	)
 	console.log(`Bot: user is ${userName}`)
 	const job = async () => {
 		console.log('JobRunner: job start')
-		await login()
+		if (await login(page)) {
+			await persistCookies(page, cookiePath)
+		}
 		const time = await perkUpgrade(page, DefaultUpgradingStrategy)
 		console.log(`JobRunner: next job will run after ${time / 1000} seconds`)
 		console.log('JobRunner: job done')
@@ -78,7 +105,7 @@ let browser: Browser
 		await job()
 	}
 	await job()
-})().catch(err => {
+})().catch((err) => {
 	console.error(err)
 	console.log('Bot: unexpected error occurred')
 })
