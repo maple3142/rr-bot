73f7dcd4745d98c3b849562f456effe89f39994d
maple3142 <kirby741852963@gmail.com>
2020-04-13 12:11:07 +0800

    fix perk upgrade

--- src/perk-upgrade.ts
+++ src/perk-upgrade.ts
@@ -36,7 +36,7 @@ async function getDurationString(page: Page): Promise<string | null> {
 	const counter = await page.$('#perk_counter_2')
 	if (counter) {
 		const duration = ((await counter.evaluate(
-			el => el.textContent
+			(el) => el.textContent
 		)) as string).trim()
 		return duration
 	}
@@ -46,34 +46,35 @@ export default async (page: Page, strategy: PerkUpgradingStrategy) => {
 	const duration = await getDurationString(page)
 	if (duration) {
 		console.log(`PerkUpgrade: remaining ${duration}`)
-		return parseTime(duration) * 1000
+		return parseTime(duration) * 1000 + 10000 // plus 10s
 	}
 	await page.waitFor('.perk_source_2')
 	const str = (await page.$eval(
 		'.perk_source_2[perk="1"]',
-		el => el.textContent
+		(el) => el.textContent
 	)) as string
 	const edu = (await page.$eval(
 		'.perk_source_2[perk="2"]',
-		el => el.textContent
+		(el) => el.textContent
 	)) as string
 	const end = (await page.$eval(
 		'.perk_source_2[perk="3"]',
-		el => el.textContent
+		(el) => el.textContent
 	)) as string
 	const upg = strategy.determineWhichToUpgrade(
 		parseInt(str.trim()),
 		parseInt(edu.trim()),
 		parseInt(end.trim())
 	)
+	console.log(
+		`PerkUpgrade: upgrade ${upg === 1 ? 'STR' : upg === 2 ? 'EDU' : 'END'}`
+	)
 	const perkEntrySelector = `#index_perks_list>.perk_item[perk="${upg}"]`
 	await page.waitFor(perkEntrySelector)
-	await page.$eval(perkEntrySelector, el => (<any>el).click())
+	await page.$eval(perkEntrySelector, (el) => (<any>el).click())
 	const upgradeBtnSelector = '#perk_target_4>.perk_select>.perk_3>div'
 	await page.waitFor(upgradeBtnSelector)
-	await page.$eval(upgradeBtnSelector, el => (<any>el).click())
+	await page.$eval(upgradeBtnSelector, (el) => (<any>el).click())
 	await page.waitFor('#perk_counter_2')
-	const newDuration = (await getDurationString(page)) as string
-	console.log(`PerkUpgrade: remaining ${newDuration}`)
-	return parseTime(newDuration) * 1000
+	return 300 * 1000 // wait for 5 minutes and check the time
 }
